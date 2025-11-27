dfabf2e640239edbe37c791754a5731618a75592
felgines <sara@MacBookAir.lan>
2025-11-27 22:44:19 +0100

    Ajout du Pareto, Alpha Beta Delta, et affichage 3D

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import numpy as np
 from utils import load_data
 from video_scheduler import Donnees, generate_valid_solution
+from leader_pareto import *
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D 
 
 if __name__ == "__main__":
 
-    # Création de l'instance des données
-    videos, vms = load_data("datasets/videos.csv","datasets/machines_virtuelles.csv")
+    videos, vms = load_data("datasets/videos_reduit.csv","datasets/machines_virtuelles_reduit.csv")
     donnees = Donnees(videos, vms)
 
     # Générer 10 solutions valides (on changera par 100 plus tard)
@@ -19,4 +21,35 @@ if __name__ == "__main__":
         print(f"  Cout total: {solution.cost}")
         print(f"  Energie totale: {solution.energy}")
         print("----------")
+
+    
+    # ---- Affichage des fronts ----
+    fronts = generate_fronts(valid_solutions)
+    print("\n===== FRONTS NON DOMINES =====")
+    for i, front in enumerate(fronts):
+        print(f"\nFront {i+1} :")
+        for solution in front:
+            index = valid_solutions.index(solution) + 1 
+            print(f"  Solution {index} :  Makespan={solution.makespan:.4f}, Cost={solution.cost:.4f}, Energy={solution.energy:.4f}")
+    
+    # ---- Affichage des leaders ----
+    alpha, beta, delta = select_leaders(valid_solutions)
+    print("\n===== LEADERS =====")
+    for name, solution in [("Alpha", alpha), ("Beta", beta), ("Delta", delta)]:
+        if solution is None:
+            print(f"{name} : None")
+        else:
+            idx = valid_solutions.index(solution) + 1
+            print(f"{name} = Solution {idx} : "
+                  f"Makespan={solution.makespan:.4f}, "
+                  f"Cost={solution.cost:.4f}, "
+                  f"Energy={solution.energy:.4f}")
+
+    
+    
+        
+# Affichage graphique 3D
+plot_fronts_3d(valid_solutions, fronts)
+
+   
 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+nt("Makespan:", solution.makespan)
+print("Coût:", solution.cost)
+print("Énergie:", solution.energy)
